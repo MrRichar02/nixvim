b7759d1d8ed906b313eee3bedcd2b1aa96ca410f
MrRichar02 <richarmamadi03@gmail.com>
2025-05-25 18:04:35 -0500

    changing color scheme

--- config/colorScheme.nix
+++ config/colorScheme.nix
@@ -1,12 +1,12 @@
 {
-  # colorschemes.catppuccin = {
-  #   enable = true;
-  #   settings = {
-  #     flavour = "mocha";
-  #     background.dark = "mocha";
-  #   };
-  # };
-  colorschemes.gruvbox = {
+  colorschemes.catppuccin = {
     enable = true;
+    settings = {
+      flavour = "mocha";
+      background.dark = "mocha";
+    };
   };
+  # colorschemes.gruvbox = {
+  #   enable = true;
+  # };
 }
